d23e4eebb7b7691a0484b62f825f7ac30cb6cb4c
vasa1510 <vasa9865@gmail.com>
2025-11-03 05:16:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: 'dist' }
+  css: {
+    postcss: path.resolve(__dirname, 'postcss.config.js'),
+  },
+  build: {
+    outDir: 'dist',
+  },
 })
